e06c1d3eaf1f12449753e7e796d227e4d2d0142b
Eylexander <eylexander88@gmail.com>
2025-11-03 10:49:41 +0100

    Implementation de la certification cyan

--- engine.js
+++ engine.js
@@ -2,6 +2,7 @@ var Engine = {
     running: false,
     check: function () {
         console.log("Engine Ready !")
+        console.log("Cyan level checked".cyan)
         return true
     },
     start: function () {
