9d79a6fec1ff3feecbf64df57b65425559569e55
39soup <145912675+39soup@users.noreply.github.com>
2025-12-03 11:12:33 +0100

    parser

--- CruParser.js
+++ CruParser.js
@@ -1,30 +1,58 @@
-var course = require('./course');
+var { Course, Slot } = require('./course');
+
+// for testing purposes
+const fs = require('fs');
+fs.readFile(('./data/AB/edt.cru'), 'utf8', function (err,data) {
+            if (err) {
+                return err;
+            }
+      
+            var analyzer = new CruParser(true, true);
+            analyzer.parse(data);
+            
+            if(analyzer.errorCount === 0){
+                console.log("The .cru file is a valid cru file");
+            }else{
+                console.log("The .cru file contains error");
+            }
+            
+            console.log(analyzer.parsedCourse);
+
+        });
 
 
 // CruParser
 
-var CruParser = function(sTokenize, sParsedSymb){
+var CruParser = function(sTokenize, sParsedSymbols){
 	// The list of course parsed from the input file.
 	this.parsedCourse = [];
-	this.symb = ["START_POI","name","latlng","note","END_POI","$$"];
-	this.showTokenize = sTokenize;
-	this.showParsedSymbols = sParsedSymb;
+    this.symb = ["+","P=","H=","S=","//", "/"];
+	this.showTokenize = sTokenize; // true to show the tokenized data
+    this.showParsedSymbols = sParsedSymbols; // true to show parsed symbols during the parsing
 	this.errorCount = 0;
 }
 
 // Parser procedure
 
 // tokenize : tranform the data input into a list
-// <eol> = CRLF
 CruParser.prototype.tokenize = function(data){
-	var separator = /(\r\n|: )/;
-	data = data.split(separator);
-	data = data.filter((val, idx) => !val.match(separator)); 					
-	return data;
+	var separator = /\/\/|\+|P=|H=|S=|[A-Za-z0-9:]+/g;
+	data = data.match(separator);
+	const tokens = []
+	/* for (let i = 0; i < data.length; i++) {
+		if (data[i] === "P=" || data[i] === "H=" || data[i] === "S=") continue; // retirer les P=, H=, S=
+		tokens.push(data[i]);
+	} */
+	
+	return tokens;
 }
 
-// parse : analyze data by calling the first non terminal rule of the grammar
+
+
+
+// parse
 CruParser.prototype.parse = function(data){
+// parse : analyze data by calling the first non terminal rule of the grammar
 	var tData = this.tokenize(data);
 	if(this.showTokenize){
 		console.log(tData);
@@ -85,23 +113,22 @@ CruParser.prototype.expect = function(s, input){
 // Parser rules
 
 // <liste_poi> = *(<poi>) "$$"
-CruParser.prototype.listPoi = function(input){
-	this.poi(input);
+CruParser.prototype.listCourse = function(input){
+	this.course(input);
 	this.expect("$$", input);
 }
 
 // <poi> = "START_POI" <eol> <body> "END_POI"
-CruParser.prototype.poi = function(input){
+CruParser.prototype.course = function(input){
 
-	if(this.check("START_POI", input)){
-		this.expect("START_POI", input);
+	if(this.check("+", input)){
+		this.expect("+", input);
 		var args = this.body(input);
-		var p = new POI(args.nm, args.lt, args.lg, []);
-		this.note(input, p);
-		this.expect("END_POI",input);
-		this.parsedPOI.push(p);
+		var p = new Course(args.nm, args.sl);
+		this.expect("//",input);
+		this.parsedCourse.push(p);
 		if(input.length > 0){
-			this.poi(input);
+			this.course(input);
 		}
 		return true;
 	}else{
@@ -110,29 +137,29 @@ CruParser.prototype.poi = function(input){
 
 }
 
-// <body> = <name> <eol> <latlng> <eol> <optional>
+// CoursData = "+" CodeCours CRLF Slot+
 CruParser.prototype.body = function(input){
 	var nm = this.name(input);
-	var ltlg = this.latlng(input);
-	return { nm: nm, lt: ltlg.lat, lg: ltlg.lng };
+	var sl = this.slot(input);
+	return { nm: nm, sl: sl };
 }
 
-// <name> = "name: " 1*WCHAR
+// CodeCours = 3*8(ALPHA / DIGIT)
 CruParser.prototype.name = function(input){
-	this.expect("name",input)
+	this.expect("+",input);
 	var curS = this.next(input);
-	if(matched = curS.match(/[\wàéèêîù'\s]+/i)){
+	if(matched = curS.match(/[A-Za-z0-9]{3,8}/)){
 		return matched[0];
 	}else{
 		this.errMsg("Invalid name", input);
 	}
 }
 
-// <latlng> = "latlng: " ?"-" 1*3DIGIT "." 1*DIGIT", " ?"-" 1*3DIGIT "." 1*DIGIT
-CruParser.prototype.latlng = function(input){
-	this.expect("latlng",input)
+// Slot = Index "," Type ",P=" Capacite",H=" Jour SP HeureDeb "-" HeureFin",F" SousGrp",S="Salle "//" CRLF
+CruParser.prototype.slot = function(input){
+	this.expect("1",input); // slots start with 1
 	var curS = this.next(input);
-	if(matched = curS.match(/(-?\d+(\.\d+)?);(-?\d+(\.\d+)?)/)){ // matches both positive and negative coordinates
+	if(matched = curS.match(/(-?\d+(\.\d+)?);(-?\d+(\.\d+)?)/)){ 
 		return { lat: matched[1], lng: matched[3] };
 	}else{
 		this.errMsg("Invalid latlng", input);
@@ -155,5 +182,6 @@ CruParser.prototype.note = function (input, curPoi){
 		}	
 	}
 }
+
 
 module.exports = CruParser;
--- course.js
+++ course.js
@@ -1,8 +1,19 @@
-var course = function(nm, sl){
-	this.name = nm;
-	this.slots = sl;
+// course.js
+var Course = function(name, slots){
+	this.name = name;
+	this.slots = slots;
 }
 	
+var Slot = function(type, capacity, day, start, end, subgroup, room) {
+    this.type = type;
+    this.capacity = capacity;
+    this.day = day;
+    this.start = start;
+    this.end = end;
+    this.subgroup = subgroup;
+    this.room = room;
 
+};
 
-module.exports = course;
+module.exports.Course = Course;
+module.exports.Slot = Slot;
